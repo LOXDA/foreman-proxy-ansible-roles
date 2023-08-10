2588169fefd17d128049d0ecb817ff2481f90619
Thomas Basset <tomm@loxda.net>
2023-08-11 01:04:14 +0200

    cleanup

--- run_update.sh
+++ run_update.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
 git config pull.rebase false
 git config http.sslverify false
 git config credential.helper cache
 git pull
-git submodule update --remote --recursive --init
-#git gc
+git submodule update --init
+git gc
